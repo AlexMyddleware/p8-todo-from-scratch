b6c27d344cb8d868ff1670119cc8fdcc0ed7667a
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2023-07-21 10:15:59 +0200

    feat: working login, working home page style

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -17,7 +17,7 @@ class LoginController extends AbstractController
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
-        return $this->render('login/index.html.twig', [
+        return $this->render('security/login.html.twig', [
             'controller_name' => 'LoginController',
             'last_username' => $lastUsername,
             'error'         => $error,
