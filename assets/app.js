8d7da31259c1249237b56c202c48b665c9cf3935
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2023-07-22 13:57:06 +0200

    feat: remove snowtricks elements

--- assets/app.js
+++ assets/app.js
@@ -6,11 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+// import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
-import './Form/ImageLink';
-import './Form/VideoLink';
-import './phone';
-import './load-more';
