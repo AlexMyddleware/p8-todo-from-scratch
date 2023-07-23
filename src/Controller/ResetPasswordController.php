042a6ae8f6eb86511e317378c8696fd505712dd5
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2023-07-23 10:55:40 +0200

    feat: flash message when works

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -121,6 +121,8 @@ class ResetPasswordController extends AbstractController
             // The session is cleaned up after the password has been changed.
             $this->cleanSessionAfterReset();
 
+            // add flash message
+            $this->addFlash('success', 'Votre mot de passe a été modifié avec succès.');
             return $this->redirectToRoute('app_home');
         }
 
