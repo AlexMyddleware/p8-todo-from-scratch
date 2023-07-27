f63f5b9fa8e9813d107af9e8944b6ed8ee0530bb
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2023-07-27 08:18:05 +0200

    feat: update entity and repo

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -4,17 +4,84 @@ namespace App\Entity;
 
 use App\Repository\TaskRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: TaskRepository::class)]
 class Task
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
+    #[ORM\GeneratedValue(strategy: "AUTO")]
+    #[ORM\Column(type: "integer")]
     private ?int $id = null;
+
+    #[ORM\Column(type: "datetime", options: ["default" => "CURRENT_TIMESTAMP"])]
+    private \DateTimeInterface $createdAt;
+
+    #[ORM\Column(type: "string")]
+    #[Assert\NotBlank(message: "Vous devez saisir un titre.")]
+    private string $title;
+
+    #[ORM\Column(type: "text")]
+    #[Assert\NotBlank(message: "Vous devez saisir du contenu.")]
+    private string $content;
+
+    #[ORM\Column(type: "boolean")]
+    private bool $isDone;
+
+    public function __construct(
+        string $title,
+        string $content,
+        bool $isDone = false,
+        ?\DateTimeInterface $createdAt = null
+    ) {
+        $this->title = $title;
+        $this->content = $content;
+        $this->isDone = $isDone;
+        $this->createdAt = $createdAt ?? new \DateTimeImmutable();
+    }
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getCreatedAt(): \DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): void
+    {
+        $this->content = $content;
+    }
+
+    public function isDone(): bool
+    {
+        return $this->isDone;
+    }
+
+    public function toggle(bool $flag): void
+    {
+        $this->isDone = $flag;
     }
 }
