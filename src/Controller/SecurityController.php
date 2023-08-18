7f63ef7c04b147d624913a96802075bfe2223a57
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2023-08-18 21:53:42 +0200

    feat: coverage, remove unnecessary dump

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,10 +6,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SecurityController extends AbstractController
 {
-    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    /**
+     * @Route("/logout", name="app_logout", methods={"GET"})
+     * 
+     * @codeCoverageIgnore
+     */
     public function logout(): void
     {
-        // controller can be blank: it will never be called!
-        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+
     }
 }
